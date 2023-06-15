a29610d65a187adcb46cada1a3a5478f391a30c2
Mark <scaperbet@gmail.com>
2023-06-15 17:45:41 +0300

    добавлено модальное окно

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -30,8 +30,9 @@ console.log(mobileCheck())
 // import { enableScroll } from './functions/enable-scroll';
 
 // Реализация модального окна
-// import GraphModal from 'graph-modal';
-// const modal = new GraphModal();
+import GraphModal from 'graph-modal';
+const modal = new GraphModal();
+
 
 // Реализация табов
 // import GraphTabs from 'graph-tabs';
